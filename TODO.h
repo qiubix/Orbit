0f7e526111469e5bf8297446302ba312f15ec0bd
qiubix <qiubix@gmail.com>
2014-04-09 13:17:22 +0200

    Add TODO file

---
+++ TODO.h
@@ -0,0 +1,60 @@
+/*
+
+Dopisuj tutaj rzeczy do zrobienia w kolejnych iteracjach, a ja je potem ewentualnie dodam na stronie projektu,
+to siê bêd¹ wyœwietlaæ w team explorerze. Da siê te¿ wyœwietliæ rzeczy do zrobienia i je edytowaæ z poziomu VS
+(oczywiœcie pod³¹czonego do projektu :P ). Dziêki temu bêdziemy wiedzieæ, co jest zrobione, a co nie. Ten plik
+stworzy³em na wszelki wypadek, jako bardziej tradycyjn¹ formê porz¹dkowania rzeczy do zrobienia. Moja propozycja 
+jest taka, ¿eby po zrobieniu czegoœ z listy nie usuwaæ tego, tylko zamieniaæ '-' na '+'. Ale oczywiœcie mo¿na te¿
+napisaæ sobie na skypie, co robimy albo po prostu robiæ i nic nie ustalaæ. 
+
+
+TODO:
+
+=================== SPRINT 0 ============================
+
+Zmieñ proszê nazwê wyœwietlania swojego profilu z artus.sawicki@gmail.com na coœ innego, nieco krótszego, np swoje imiê. 
+Na stronie projektu/swojej stronie TFS w prawym górnym rogu naciskasz na swoj¹ nazwê, wybierasz "My Profile" i przy polu "Display Name"
+edytujesz. 
+
+=================== SPRINT 1 ============================
+WIDOK:
+	- wyœwietlanie planszy
+	- wyœwietlanie komety
+	- wyœwietlanie planet
+
+KONTROLER: 
+	- update_model();
+	- change_game_state(Game_state .. );
+	- prepare_next_level();
+
+
+=================== SPRINT 2 ============================
+
+WIDOK:
+	- wyœwietlanie numeru levelu
+	- wyœwietlanie paska up³ywu czasu
+
+KONTROLER:
+	- wykrywanie ruchu myszki
+
+
+=================== SPRINT 3 ============================
+
+
+
+=================== SPRINT 4 ============================
+
+KONTROLER:
+	- funkcja generuj¹ca próbki
+	- funkcja licz¹ca wspó³rzêdne naciagniêcia
+	- funkcja przystosowania
+	- funkcja testuj¹ca populacjê
+
+
+=================== SPRINT 5 ============================
+
+
+
+=================== SPRINT 6 ============================
+
+*/
